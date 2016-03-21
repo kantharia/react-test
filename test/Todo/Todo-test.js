34ae90fdd359a158b86ed46c7adef3fd2f0a4538
Chetan Kantharia <chetan@chetan-desktop.local>
2016-03-21 12:53:23 +0530

    test: added check for The todo does not have the done class

--- test/Todo/Todo-test.js
+++ test/Todo/Todo-test.js
@@ -13,17 +13,24 @@ function shallowRenderTodo(todo) {
 
 test('Todo component', (t) => {
   t.test('rendering a todo', (t) => {
-    const todo = { id: 1, name: 'Buy Milk', done: true};
-    const result = shallowRenderTodo(todo);
+    const todo1 = { id: 1, name: 'Buy Milk', done: true};
+    const todo2 = { id: 1, name: 'Buy Milk', done: false};
+    const result1 = shallowRenderTodo(todo1);
+    const result2 = shallowRenderTodo(todo2);
 
     t.test('It renders the text of the todo', (t) => {
       t.plan(1);
-      t.equal(result.props.children[0].props.children, 'Buy Milk');
+      t.equal(result1.props.children[0].props.children, 'Buy Milk');
+    });
+
+    t.test('The todo does not have the done class', (t) => {
+      t.plan(1);
+      t.equal(result2.props.className.indexOf('done-todo'),-1)
     });
 
     t.test('The todo does have the done class', (t) => {
       t.plan(1);
-      t.ok(result.props.className.indexOf('done-todo') > -1);
+      t.ok(result1.props.className.indexOf('done-todo') > -1);
     })
   });
 });
